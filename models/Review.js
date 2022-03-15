b462fc698eeece470c0a3fa265edaae0dd794902
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-15 06:49:14 +0530

    aggregate used to auto update number of reviews and average rating

--- models/Review.js
+++ models/Review.js
@@ -31,5 +31,40 @@ const ReviewSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" },
+        numberOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+  console.log(result);
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numberOfReviews: result[0]?.numberOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+ReviewSchema.post("save", async function (req, res) {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+ReviewSchema.post("remove", async function (req, res) {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
 module.exports = mongoose.model("Review", ReviewSchema);
