5fab72f324a746cfe8feed2ac5b9d321232b594a
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-15 23:42:49 +0530

    added order schema and controllers

---
+++ routes/orderRoutes.js
@@ -0,0 +1,29 @@
+const {
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  createOrder,
+  updateOrder,
+} = require("../controllers/orderController");
+
+const express = require("express");
+const router = express.Router();
+
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
+
+router
+  .route("/")
+  .get(authenticateUser, authorizePermissions("admin"), getAllOrders)
+  .post(authenticateUser, createOrder);
+
+router.route("/showAllMyOrders").get(authenticateUser, getCurrentUserOrders);
+
+router
+  .route("/:id")
+  .get(authenticateUser, getSingleOrder)
+  .patch(authenticateUser, updateOrder);
+
+module.exports = router;
