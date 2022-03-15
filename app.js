5a7ddc3e11b2b58c12ba2a8b48a07e3be15e0227
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-03-16 01:44:13 +0530

    security pakages added

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 //port
 const port = process.env.PORT || 5000;
 
@@ -22,6 +27,20 @@ const connectDB = require("./db/connect");
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//security pakages
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowsMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 //using morgan
 app.use(morgan("tiny"));
